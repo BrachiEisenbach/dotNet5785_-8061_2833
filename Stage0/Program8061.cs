1e40ca4aa62319ebd4733394f85bf0fdd740dd90
BrachiEisenbach <brachie394@gmail.com>
2024-11-13 22:27:08 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program8061.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+namespace Targil0 { 
+
+
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome8061();
+        Welcome2833();
+        Console.ReadKey();
+
+    }
+    static partial void Welcome2833();
+    private static void Welcome8061()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine(name + ", welcome to my first console application");
+    }
+}
+}
