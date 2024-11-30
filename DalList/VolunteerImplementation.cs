a49a7bb04196965650fc0fd3eee98daf0bf2ef53
BrachiEisenbach <brachie394@gmail.com>
2024-11-30 19:01:08 +0200

    שליחה לאחר התחלת ה main

--- DalList/VolunteerImplementation.cs
+++ DalList/VolunteerImplementation.cs
@@ -4,7 +4,7 @@ using DalApi;
 using DO;
 using System.Collections.Generic;
 
-internal class VolunteerImplementation : IVolunteer
+public class VolunteerImplementation : IVolunteer
 {
     public void Create(Volunteer item)
 
