bbb0193387e87a5019012e0798f876bb42884ede
BrachiEisenbach <brachie394@gmail.com>
2024-12-08 17:49:23 +0200

    תיעוד לממשקים ברכי

--- DalList/VolunteerImplementation.cs
+++ DalList/VolunteerImplementation.cs
@@ -4,8 +4,17 @@ using DalApi;
 using DO;
 using System.Collections.Generic;
 
+/// <summary>
+/// A class for implementing the volunteer's interface.
+/// </summary>
+
 public class VolunteerImplementation : IVolunteer
 {
+    /// <summary>
+    /// A function to add a newly created Volunteer to the Volunteer list.
+    /// </summary>
+    /// <param name="item">Gets a Volunteer created in the main program.</param>
+
     public void Create(Volunteer item)
 
     {
@@ -17,6 +26,12 @@ public class VolunteerImplementation : IVolunteer
         DataSource.Volunteers.Add(item);
     }
 
+    /// <summary>
+    /// A function for deleting a Volunteer by ID.
+    /// </summary>
+    /// <param name="id">Gets a Volunteer ID.</param>
+    /// <exception cref="Exception">Throws an error in case of empty input.</exception>
+
     public void Delete(int id)
     {
         if (Read(id) == null)
@@ -27,10 +42,20 @@ public class VolunteerImplementation : IVolunteer
         DataSource.Volunteers.RemoveAll(i => i.Id == id);
     }
 
+    /// <summary>
+    /// A function to delete the entire list of data present in Volunteer type memory.
+    /// </summary>
+
     public void DeleteAll()
     {
         DataSource.Volunteers.Clear();
     }
+
+    /// <summary>
+    /// A function to find and return a Volunteer by ID.
+    /// </summary>
+    /// <param name="id">Volunteer ID.</param>
+    /// <returns>Returns the requested assignment.</returns>
 
     public Volunteer? Read(int id)
     {
@@ -39,11 +64,22 @@ public class VolunteerImplementation : IVolunteer
         //throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// A function that returns a list of all Volunteer type data present in memory.
+    /// </summary>
+    /// <returns>Returns a list of data of the requested type.</returns>
+
     public List<Volunteer> ReadAll()
     {
         List<Volunteer?> v_Volunteers = DataSource.Volunteers;
         return new List<Volunteer?>(v_Volunteers);
     }
+
+    /// <summary>
+    /// A function to update an existing Volunteer by ID.
+    /// </summary>
+    /// <param name="item">An existing Volunteer.</param>
+    /// <exception cref="Exception">Returns an error in case of empty input.</exception>
 
     public void Update(Volunteer item)
     {
