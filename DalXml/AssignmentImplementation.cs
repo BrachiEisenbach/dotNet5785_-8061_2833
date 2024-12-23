d26898611cdffe58d4ebe555829f6125885d1848
BrachiEisenbach <brachie394@gmail.com>
2024-12-23 20:49:52 +0200

    תחילת 6 ב לא גמור

--- DalXml/AssignmentImplementation.cs
+++ DalXml/AssignmentImplementation.cs
@@ -4,9 +4,23 @@ using DalApi;
 using DO;
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 internal class AssignmentImplementation : IAssignment
 {
+    static Assignment getAssignment(XElement s)
+    {
+        return new DO.Assignment()
+        {
+            Id = s.ToIntNullable("Id") ?? throw new FormatException("can't convert id"),
+            CallId = s.ToIntNullable("CallId") ?? throw new FormatException("can't convert id"),
+            VolunteerId = s.ToIntNullable("VolunteerId") ?? throw new FormatException("can't convert id"),
+            EntryTimeForTreatment = (DateTime)s.Element("EntryTimeForTreatment"),
+            EndTimeOfTreatment = (DateTime)s.Element("EndTimeOfTreatment"),
+            TypeOfTreatment = s.ToDateTimeNullable<EndTimeOfTreatment>("TypeOfTreatment") ?? EndTimeOfTreatment.
+        };
+    }
+
     public void Create(Assignment item)
     {
         throw new NotImplementedException();
@@ -14,23 +28,29 @@ internal class AssignmentImplementation : IAssignment
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        List<Assignment> Courses = XMLTools.LoadListFromXMLSerializer<Assignment>(Config.s_assignment_xml);
+        if (Courses.RemoveAll(it => it.Id == id) == 0)
+            throw new DalDoesNotExistException("Assignment", $"Course with ID={id} does Not exist");
+        XMLTools.SaveListToXMLSerializer(Courses, Config.s_assignment_xml);
     }
 
     public void DeleteAll()
     {
-        throw new NotImplementedException();
+        XMLTools.SaveListToXMLSerializer(new List<Assignment>(), Config.s_assignment_xml);
     }
 
     public Assignment? Read(int id)
     {
-        throw new NotImplementedException();
+        XElement? studentElem =
+    XMLTools.LoadListFromXMLElement(Config.s_students_xml).Elements().FirstOrDefault(st => (int?)st.Element("Id") == id);
+        return studentElem is null ? null : getStudent(studentElem);
     }
 
     public Assignment? Read(Func<Assignment, bool> filter)
     {
-        throw new NotImplementedException();
+        return XMLTools.LoadListFromXMLElement(Config.s_students_xml).Elements().Select(s => getStudent(s)).FirstOrDefault(filter);
     }
+
 
     public IEnumerable<Assignment> ReadAll(Func<Assignment, bool>? filter = null)
     {
@@ -39,6 +59,10 @@ internal class AssignmentImplementation : IAssignment
 
     public void Update(Assignment item)
     {
-        throw new NotImplementedException();
+        List<Assignment> Courses = XMLTools.LoadListFromXMLSerializer<Assignment>(Config.s_assignment_xml);
+        if (Courses.RemoveAll(it => it.Id == item.Id) == 0)
+            throw new DalDoesNotExistException("Assignment",$"Course with ID={item.Id} does Not exist");
+        Courses.Add(item);
+        XMLTools.SaveListToXMLSerializer(Courses, Config.s_assignment_xml);
     }
 }
