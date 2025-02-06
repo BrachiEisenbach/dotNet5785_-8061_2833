acba2f8617c709848d3495e618ce6e1bf7db3b7e
BrachiEisenbach <brachie394@gmail.com>
2025-02-06 21:28:11 +0200

    אמצע 8 א

---
+++ BL/BlApi/ICall.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi
+{
+    public interface ICall
+    {
+        public int[] GetCallCountsByStatus();
+        public void UpdateCallStatus();
+        public BO.CallInList GetCallList(Enum? STATUS, object? valFilter, Enum? TYPEOFCALL);
+        public BO.Call GetCallDetails(int callId);
+        public void UpdateCallDetails(BO.Call call);
+        public void DeleteCall(int callId);
+        public void AddCall(BO.Call call);
+
+
+    }
+}
