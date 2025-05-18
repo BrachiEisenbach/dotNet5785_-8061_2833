0b7310a26b0b381f644ddbd820bc5f87c7b2f173
neotal <neomital120@gmail.com>
2025-05-19 00:03:47 +0300

    לא עבד לי לעשות פוש..

---
+++ PL/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+        public DateTime CurrentTime
+        {
+            get { return (DateTime)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty, value); }
+        }
+        public static readonly DependencyProperty CurrentTimeProperty =
+    DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow));
+
+
+        private void btnAddOneMinute_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ClockPromotion(BO.TIMEUNIT.MINUTE);
+        }
+        private void btnAddOneDay_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ClockPromotion(BO.TIMEUNIT.DAY);
+        }
+        private void btnAddOneHour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ClockPromotion(BO.TIMEUNIT.HOUR);
+        }
+        private void btnAddOneMonth_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ClockPromotion(BO.TIMEUNIT.MONTH);
+        }
+        private void btnAddOneYear_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ClockPromotion(BO.TIMEUNIT.YEAR);
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
