d67ca4f2838af6669efa292327d833ed03f61223
BrachiEisenbacl <brachie394@gmail.com>
2025-05-20 17:03:54 +0300

    אמצע שלב 8 ב

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -4,19 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BO;
 
 namespace PL
 {
-    //internal class Enums
-    //{   
+
+    internal class TypeOfCallCollection : IEnumerable
+    {
+        static readonly IEnumerable<BO.TYPEOFCALL> s_enums =
+    (Enum.GetValues(typeof(BO.TYPEOFCALL)) as IEnumerable<BO.TYPEOFCALL>)!;
+
+        public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+        public BO.TYPEOFCALL volunteer { get; set; } = BO.TYPEOFCALL.REDRIVE;
+    }
+
+    //public class TypeOfCallCollection : IEnumerable<TYPEOFCALL>
+    //{
+    //    private readonly List<TYPEOFCALL> _items; // רשימה של TYPEOFCALL
+
+    //    public TypeOfCallCollection()
+    //    {
+    //        _items = new List<TYPEOFCALL>
+    //        {
+    //            // הוסף את הפריטים כאן
+    //        };
+    //    }
+
+    //    // מימוש של GetEnumerator עבור IEnumerable<T>
+    //    public IEnumerator<TYPEOFCALL> GetEnumerator() => _items.GetEnumerator();
+
+    //    // מימוש של GetEnumerator עבור IEnumerable
+    //    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     //}
-
-        internal class SemestersCollection : IEnumerable
-        {
-            static readonly IEnumerable<BO.VOLUNTEERFIELDSORT> s_enums =
-        (Enum.GetValues(typeof(BO.VOLUNTEERFIELDSORT)) as IEnumerable<BO.VOLUNTEERFIELDSORT>)!;
-
-            public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-        }
-
 }
