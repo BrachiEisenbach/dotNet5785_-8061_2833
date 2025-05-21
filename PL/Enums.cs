58c54f5f5febc2d63c06377de3c1bc39764b8e11
BrachiEisenbacl <brachie394@gmail.com>
2025-05-21 20:40:09 +0300

    איפשהו אמצע 8

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -4,34 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using BO;
 
 namespace PL
 {
-    internal class TypeOfCallCollection :  IEnumerable
+
+    public class TypeOfCallCollection : IEnumerable
     {
         static readonly IEnumerable<BO.TYPEOFCALL> s_enums =
-            (Enum.GetValues(typeof(BO.TYPEOFCALL)) as IEnumerable<BO.TYPEOFCALL>)!;
+    (Enum.GetValues(typeof(BO.TYPEOFCALL)) as IEnumerable<BO.TYPEOFCALL>)!;
 
         public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
         public BO.TYPEOFCALL volunteerTypeOfCall { get; set; } = BO.TYPEOFCALL.NONE;
+
+
+
+    //    static readonly IEnumerable<BO.TYPEOFDISTANCE> d_enums =
+    //(Enum.GetValues(typeof(BO.TYPEOFDISTANCE)) as IEnumerable<BO.TYPEOFDISTANCE>)!;
+
+    //    public IEnumerator GetEnumerator() => d_enums.GetEnumerator();
+    //    public BO.TYPEOFDISTANCE volunteerTypeOfDistance { get; set; } = BO.TYPEOFDISTANCE.NONE;
+
+
+
+
     }
+    
 
-    internal class RoleCollection :  IEnumerable
-    {
-        static readonly IEnumerable<BO.ROLE> s_enums =
-            (Enum.GetValues(typeof(BO.ROLE)) as IEnumerable<BO.ROLE>)!;
 
-        public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-       
-    }
-
-    internal class TypeOfDistanceCollection : IEnumerable
-    {
-        static readonly IEnumerable<BO.TYPEOFDISTANCE> s_enums =
-            (Enum.GetValues(typeof(BO.TYPEOFDISTANCE)) as IEnumerable<BO.TYPEOFDISTANCE>)!;
-
-        public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-    }
 }
